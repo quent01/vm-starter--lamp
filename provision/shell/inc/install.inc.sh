d5788fccaaf160f1fa2a8ba213b61cd6e056612b
qthenoz <tech_director@tiz.fr>
2020-06-06 10:26:39 +0200

    install wp-cli

--- provision/shell/inc/install.inc.sh
+++ provision/shell/inc/install.inc.sh
@@ -456,10 +456,10 @@ function install_beanstalkd(){
 # ==============================*/
 function install_wpcli(){
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-    sudo chmod +x wp-cli.phar
     sudo mv wp-cli.phar /usr/local/bin/wp
+    sudo chmod +x /usr/local/bin/wp
 }
-# install_wpcli
+install_wpcli
 
 
 
